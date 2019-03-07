a52747457bac772e5d9d787126c93657eb7f43cf
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-07 14:44:43 +0100

    Ajout RecipesManager.php au repo contenant méthodes getListRecipes() et getRecipe()

---
+++ model/RecipesManager.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'Manager.php';
+
+class RecipesManager extends Manager {
+
+    public function getListRecipes() {
+
+        $db = $this->dbconnect(); // this.dbConnect()
+        $q = $db->prepare('SELECT id, title, author, DATE_FORMAT(date_creation, \'%d/%m/%Y %H:%i:%s\') AS date_fr FROM recipes ORDER BY date_creation DESC');
+        $q->execute();
+        $dataListPosts = $q->fetch();
+
+        return $dataListPosts;
+    }
+    
+    public function getRecipe($idRecipe) {
+
+        $db = $this->dbConnect();
+        $q = $db->prepare('SELECT id, title, author, content FROM recipes WHERE id = ?');
+        $q->execute(array($idRecipe));
+        $dataPost = $q->fetch(); 
+
+        return $dataPost;
+
+    }
+}
+
+
+
