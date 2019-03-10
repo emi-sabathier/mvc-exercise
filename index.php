9b08f208dacb56b35bcb9085647feb5d6a9a7572
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-10 09:09:31 +0100

    suppression des espaces en trop

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
+
 use EmilieSabathier\controller\front\FrontController;
 
 require 'controller/front/frontController.php';
 
 if (isset($_GET['action'])) {
 
+    $recipesManager = new FrontController();
+
     if ($_GET['action'] == 'listrecipes') {
-
-        $recipesManager = new FrontController();
         $recipesManager->listRecipes();
-
     } elseif ($_GET['action'] == 'recipe') {
-
-        $recipesManager = new FrontController();
         $recipesManager->displayRecipe($_GET['idRecipe']);
-
-    } elseif ($_GET['action'] == "") {
-
+    }  else {
         header('Location: index.php');
-
-    } 
-    
+    }
 } else {
-
+    
     $recipesManager = new FrontController();
     $recipesManager->listRecipes();
 
